10683c827729568f76bf9b9ff4c639da90dc5e30
thaydatdtlx-hub <thaydat.dtlx@gmail.com>
2026-08-04 08:47:21 +0700

    Kích hoạt video YouTube trên trang đăng ký học lái xe

--- new-student-registration.js
+++ new-student-registration.js
@@ -1,4 +1,5 @@
 import "./license-training-details.js";
+import "./training-video-section.js";
 
 const SUPABASE_URL="https://pkzxkvcncipfszeukpwu.supabase.co";
 const SUPABASE_KEY="sb_publishable_rrQ2fAG7ZpIKizN3-tss1w_4xPxq3Vo";
