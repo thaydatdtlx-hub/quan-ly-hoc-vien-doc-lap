cbe0b5f7a54f1afde77a766721310cff158eee99
thaydatdtlx-hub <thaydat.dtlx@gmail.com>
2026-08-08 15:47:16 +0700

    Load taplai-inspired Thay Dat theme

--- new-student-registration.js
+++ new-student-registration.js
@@ -1,3 +1,5 @@
+import "./taplai-inspired.css";
+import "./taplai-inspired.js";
 import "./license-training-details.js";
 import "./license-eligibility-section.js";
 import "./training-video-section.js";
