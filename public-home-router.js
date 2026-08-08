0ed199362c363c79723015095373eaff6cf7dd32
thaydatdtlx-hub <thaydat.dtlx@gmail.com>
2026-08-08 15:54:53 +0700

    Route public visitors to marketing homepage

---
+++ public-home-router.js
@@ -0,0 +1,8 @@
+const ROOT_PATHS=new Set(["/","/index.html"]);
+const params=new URLSearchParams(location.search);
+const explicitLogin=params.get("login")==="1";
+const activeToken=localStorage.getItem("hv_token")||sessionStorage.getItem("hv_token");
+
+if(ROOT_PATHS.has(location.pathname)&&!explicitLogin&&!activeToken){
+  location.replace("/dang-ky-hoc-lai-xe.html");
+}
