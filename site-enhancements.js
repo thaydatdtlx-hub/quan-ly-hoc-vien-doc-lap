9e5b566491fe41848a3492b6e70ebf13eb007c44
thaydatdtlx-hub <thaydat.dtlx@gmail.com>
2026-08-08 20:48:27 +0700

    Load admin tuition settings

--- site-enhancements.js
+++ site-enhancements.js
@@ -6,6 +6,7 @@ import "./new-student-practice-link.js";
 import "./student-training-actions.js";
 import "./schedule-stat-links.js";
 import "./student-payment-navigation.js";
+import "./admin-tuition-settings.js";
 
 let adminProfileModulesPromise=null;
 let adminProfileObserver=null;
