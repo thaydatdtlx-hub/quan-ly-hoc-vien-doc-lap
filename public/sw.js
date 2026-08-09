6997033f9050730ff67271662681959ad8848dfe
thaydatdtlx-hub <thaydat.dtlx@gmail.com>
2026-08-09 12:50:13 +0700

    refresh pwa routes for public home

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,10 @@
 function fixNoticeText(value){return String(value??"").replace(/\bng(?:à6|á6)(?=\s+\d{2}\/\d{2}\/\d{4})/giu,"ngày")}
-const CACHE_NAME="thay-dat-pwa-v30";
+const CACHE_NAME="thay-dat-pwa-v31";
 const CORE_ASSETS=[
   "/",
   "/index.html",
+  "/dang-nhap.html",
+  "/dang-ky-hoc-lai-xe.html",
   "/hoc-vien.html",
   "/lich-dao-tao.html",
   "/600-cau-hoi.html",
