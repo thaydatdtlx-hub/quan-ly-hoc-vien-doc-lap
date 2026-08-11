1708a31bbf3ec03a13408c2202c5fc87ba72d4b1
thaydatdtlx-hub <thaydat.dtlx@gmail.com>
2026-08-11 23:00:10 +0700

    load B exam set picker

--- pwa-install.js
+++ pwa-install.js
@@ -10,6 +10,7 @@ import "./student-portal-polish.js";
 import "./theory-answer-explanations.js";
 import "./theory-hero-brand.js";
 import "./mobile-public-login.js";
+import "./b-exam-set-picker.js";
 
 const DISMISS_KEY="thay_dat_pwa_install_dismissed";
 const DISMISS_DAYS=7;
